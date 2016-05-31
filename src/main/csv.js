47e6e5327181235d2b71871c4b2de58dde235a0c
Eric M Butler <ericbutler1@Erics-MacBook-Pro-2.local>
2016-05-31 11:49:41 -0400

    Suggested refactor

--- src/main/csv.js
+++ src/main/csv.js
@@ -1,31 +1,42 @@
-const _ = require('lodash');
-
-const initial = _.initial;
-const last = _.last;
-const trim = _.trim;
+const initial = require('lodash').initial;
+const last = require('lodash').last;
+const trim = require('lodash').trim;
 
 const DOUBLE_QUOTE = '"';
 const COMMA = ',';
 
-function split(input) {
-
-    function splitRecursively(parts, index, quoted) {
-        if (index === input.length) {
-            return parts;
-        } else {
-            const char = input.charAt(index);
-            if (char === DOUBLE_QUOTE) {
-                return splitRecursively(parts, index + 1, !quoted);
-            } else {
-                const newArray = char === COMMA && !quoted
-                    ? parts.concat('')
-                    : initial(parts).concat(last(parts) + char);
-                return splitRecursively(newArray, index + 1, quoted);
-            }
-        }
-    }
-
-    return splitRecursively([''], 0, false).map(trim);
+function isSeparatorComma(char, quoted) {
+	return char === COMMA && !quoted;
 }
 
-module.exports = {split: split};
+function startNewBlankItem(parts) {
+	return parts.concat('');
+}
+
+function addToLastItem(parts, char) {
+	return initial(parts).concat(last(parts) + char);
+}
+
+function split(input) {
+
+	function splitRecursively(parts, index, quoted) {
+		if (index === input.length) {
+			return parts;
+		}
+
+		const char = input.charAt(index++);
+
+		quoted = char === DOUBLE_QUOTE ? !quoted : quoted;
+		if (char !== DOUBLE_QUOTE) {
+			parts = isSeparatorComma(char, quoted) ? startNewBlankItem(parts) : addToLastItem(parts, char);
+		}
+
+		return splitRecursively(parts, index, quoted);
+	}
+
+	return splitRecursively([''], 0, false).map(trim);
+}
+
+module.exports = {
+	split: split
+};
