c6b7b0bf9e82a3f0b38bf9bde508fae9d96df7d5
Aaron Malone <aaronmalone@gmail.com>
2016-05-31 10:43:16 -0400

    Tightening up

--- src/main/csv.js
+++ src/main/csv.js
@@ -1,14 +1,13 @@
 const _ = require('lodash');
 
+const initial = _.initial;
+const last = _.last;
+const trim = _.trim;
+
 const DOUBLE_QUOTE = '"';
 const COMMA = ',';
 
 function split(input) {
-
-    function appendCharacterToLastElement(array, character) {
-        const withoutLast = array.slice(0, array.length - 1);
-        return _.concat(withoutLast, _.last(array) + character);
-    }
 
     function splitRecursively(parts, index, quoted) {
         if (index === input.length) {
@@ -17,17 +16,16 @@ function split(input) {
             const char = input.charAt(index);
             if (char === DOUBLE_QUOTE) {
                 return splitRecursively(parts, index + 1, !quoted);
-            } else if (char === COMMA && !quoted) {
-                return splitRecursively(_.concat(parts, ''), index + 1, quoted);
             } else {
-                const array = appendCharacterToLastElement(parts, char);
-                return splitRecursively(array, index + 1, quoted);
+                const newArray = char === COMMA && !quoted
+                    ? parts.concat('')
+                    : initial(parts).concat(last(parts) + char);
+                return splitRecursively(newArray, index + 1, quoted);
             }
-
         }
     }
 
-    return splitRecursively([''], 0, false).map(_.trim);
+    return splitRecursively([''], 0, false).map(trim);
 }
 
 module.exports = {split: split};
